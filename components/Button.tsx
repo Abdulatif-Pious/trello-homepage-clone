d67de6bdc3b2bb9a4daed00f18864f475463b1e6
unknown <abdulatif1215@email.com>
2023-09-06 12:16:38 +0500

    buttin fix

--- components/Button.tsx
+++ components/Button.tsx
@@ -10,7 +10,7 @@ const Button : React.FC<ButtonProps> = ({ content, color, category, widthFull })
   return (
     <button 
       className={`
-        font-medium text-[16px] text-neutral-700  rounded-md  p-[12px] my-4 cursor-pointer  z-0  transform duration-500 focus:ring
+        border-none outline-none font-medium text-[16px] text-neutral-700  rounded-md  p-[12px] my-4 cursor-pointer  z-0  transform duration-500 focus:ring
         ${color === 'violet-300' && "border border-violet-300 hover:bg-violet-300/50 ring-violet-300"} 
         ${color === "yellow-300" && "border border-yellow-300 hover:bg-yellow-300/5 ring-yellow-300"}
         ${color === "blue-600" && "text-white bg-blue-600 hover:bg-blue-700 ring-blue-300"}
